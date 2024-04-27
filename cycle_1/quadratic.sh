f55c0e687c232ebc705b8675cb746f07c0dfe356
JERRY JAMES <77136966+jerryjames2001@users.noreply.github.com>
2024-04-27 15:31:03 +0530

    Update quadratic.sh completed

--- cycle_1/quadratic.sh
+++ cycle_1/quadratic.sh
@@ -5,7 +5,7 @@ if [ $(($dis)) < 0 ]
 then
 echo "Thequadratic has no root"
 else
-root1=$(echo "scale=2; (-$b + sqrt(dis)) / (2*$a)" | bc)
-root2=$(echo "scale=2; (-$b - sqrt(dis)) / (2*$a)" | bc)
+root1=$(echo "scale=2; (-$b + sqrt($dis)) / (2*$a)" | bc)
+root2=$(echo "scale=2; (-$b - sqrt($dis)) / (2*$a)" | bc)
 echo "root of the quadratic equations are :$root1 and $root2"
 fi
